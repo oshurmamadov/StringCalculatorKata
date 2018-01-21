21cd107131e1881f08a6b873e4893fa31505d6e7
oshurmamadov <parviz0492@gmail.com>
2018-01-21 19:43:59 +0300

    Refactoring

--- src/kata/StringCalculator.kt
+++ src/kata/StringCalculator.kt
@@ -3,12 +3,13 @@ package kata
 class StringCalculator {
 
     fun add(s: String): Int {
-        if (!s.isNotEmpty())
-            return 0
-        else if(!s.contains(" "))
-            return 0
+        if (isEmptyOrHasNoSpace(s)) return 0
 
-        return s.split(" ").sumBy { it.toInt() }
+        return s.split(" ")
+                .sumBy { it.toInt() }
     }
 
+    private fun isEmptyOrHasNoSpace(s: String): Boolean {
+        return !s.isNotEmpty() || !s.contains(" ")
+    }
 }
