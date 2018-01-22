3adb562ba222f1ecb1bb18d58dcb75b599f25d8d
oshurmamadov <parviz0492@gmail.com>
2018-01-22 14:29:05 +0300

    A BIT OF REFACTORING

--- src/kata/StringCalculator.kt
+++ src/kata/StringCalculator.kt
@@ -5,8 +5,12 @@ class StringCalculator {
     fun add(s: String): Int {
         if (isEmptyOrHasNoSpace(s)) return 0
 
-        return s.split(" ")
-                .sumBy { it.toInt() }
+        val array = s.split(" ")
+
+        val a = array[0].toInt()
+        val b = array[1].toInt()
+
+        return a + b
     }
 
     private fun isEmptyOrHasNoSpace(s: String): Boolean {
